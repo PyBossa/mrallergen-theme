e909b6b7234257e7ef458c85057920e6f1993a0c
Daniel Lombraña González <teleyinex@gmail.com>
2016-06-14 11:32:07 +0200

    Show in front page.

--- static/js/searchfrontpage.js
+++ static/js/searchfrontpage.js
@@ -1,4 +1,84 @@
 $.ajax({url:'/api/result?project_id=3292'}).done(function(data){
+            if (window.matchMedia("(min-width: 768px)").matches) {
+                resultsDesktop(data);
+            } 
+            
+            else { 
+                resultsMobile(data);
+            }
+});
+
+
+function resultsDesktop(data) {
+    $(".swiper-container").remove();
+    var container = $("<div/>");
+    container.addClass("container");
+    var row = $("<div/>");
+    row.addClass("row products");
+    container.append(row);
+    $(".numbers").append(container);
+    var data = data.slice(0,4);
+    for (i=0; i<data.length;i++) {
+        if (data[i]['info']) {
+            var col = $("<div/>");
+            col.addClass("col-xs-12 col-sm-6 col-md-4 col-lg-3 div-product");
+            var product = $("<div/>");
+            product.attr("id", "product-" + i);
+            product.addClass("product");
+            var brand = $("</p>");
+            brand.addClass("brand");
+            var name = $("</p>");
+            name.addClass("name");
+            var labels = $("</p>");
+            var oneLabel = false;
+            if (data[i]['info']['labelGlutenFree'] === 'yes') {
+                oneLabel = true;
+                var iconGlutenFree = $("<img>");
+                iconGlutenFree.attr("src", "/static/img/green-gluten.svg");
+                labels.text(" Sin gluten");
+                labels.prepend(iconGlutenFree);
+                labels.addClass("gluten-free");
+                col.addClass("gluten-free ");
+                labels.addClass("classification-label");
+            }
+
+            if (data[i]['info']['ingredientsWheat'] === 'yes') {
+                oneLabel = true;
+                var iconWheat = $("<img>");
+                iconWheat.attr("src", "/static/img/red-gluten.svg");
+                labels.text(" Trigo o trazas");
+                labels.prepend(iconWheat);
+                labels.addClass("wheat");
+                col.addClass("wheat");
+                labels.addClass("classification-label");
+            }
+
+            if (!oneLabel) {
+                labels.html("<br/>");
+            }
+
+
+            product.append(brand);
+            product.append(name);
+            product.append(labels);
+
+            var divImg = $("<div/>");
+            divImg.addClass("img-vertical-centered img-vertical-home-centered");
+            var productImg = $("<img/>");
+            productImg.attr("src", data[i]['info']['productImg']);
+            productImg.addClass("img img-home img-responsive");
+            divImg.append(productImg);
+
+            name.text(data[i]['info']['name']);
+            product.append(divImg);
+            col.append(product);
+            $(".row.products").append(col);
+        }
+    }
+}
+
+
+function resultsMobile(data) {
     for(i=0;i<data.length;i++) {
         if (data[i]['info']) {
             var location = "/results?name=" + encodeURIComponent(data[i]['info']['name']);
@@ -77,6 +157,4 @@ $.ajax({url:'/api/result?project_id=3292'}).done(function(data){
         nextButton: '.swiper-button-next',
         prevButton: '.swiper-button-prev',
     });
-
-});
-
+}
