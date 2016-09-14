5dd827c94127270327029785662291d5b243bf5a
Daniel Lombraña González <teleyinex@gmail.com>
2016-09-14 15:45:22 +0200

    Remove duplicated CTA.

--- static/js/about.js
+++ static/js/about.js
@@ -1,11 +1,4 @@
 $("#aboutshowmore").on('click', function(){
-    //$(".longdescription").css("height", "100%");
     $(".longdescription").toggleClass("full");
-    if ($(".longdescription.full").length) {
-        $(this).text("Cerrar");
-    }
-    else {
-        $(this).text("Sí, porfi");
-    }
 });
 
