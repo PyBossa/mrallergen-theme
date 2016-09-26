7f5860e64f848aaf534317c197cc95ebf5ade3d1
Daniel Lombraña González <teleyinex@gmail.com>
2016-09-26 14:58:21 +0200

    refresh

--- static/js/searchresults.js
+++ static/js/searchresults.js
@@ -276,6 +276,9 @@ function search(queryData, showBig, back) {
                     low.append(txt);
                     low.append(val);
 
+
+                    $("column").html($("column").html());
+
                     //low.html('<svg width="30" height="30" viewBox="0 0 30 30" xmlns="http://www.w3.org/2000/svg"><rect class="low" x="0" y="0" width="30" height="30"/></svg><p class="small">BAJO</p><p class="small"><30</p>');
   
 
