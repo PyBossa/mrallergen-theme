ae3750cb24f3d55224d2b25edbf5176b62227b26
Daniel Lombraña González <teleyinex@gmail.com>
2016-09-27 09:44:36 +0200

    Increase spacing.

--- static/js/searchresults.js
+++ static/js/searchresults.js
@@ -286,7 +286,7 @@ function search(queryData, showBig, back) {
                     link.attr("href", data[i]['info']['url']);
                     link.text("Ver en Soysuper");
                     var warning = $("</p>");
-                    warning.html("<span class='warning-label'>NOTA</span> Recomendamos leer SIEMPRE de forma exhaustiva los listados de ingredientes impresos en los envases, previamente a la ingesta del producto por si en algun momento la información pudiera quedar desactualizada por un cambio en el proceso de fabricación. En caso de duda, consulte directamente con el fabricante.");
+                    warning.html("<span class='warning-label'>NOTA:</span> Recomendamos leer SIEMPRE de forma exhaustiva los listados de ingredientes impresos en los envases, previamente a la ingesta del producto por si en algun momento la información pudiera quedar desactualizada por un cambio en el proceso de fabricación. En caso de duda, consulte directamente con el fabricante.");
                     warning.addClass("warning");
                     var backCol = $("<div/>");
                     backCol.addClass("col-xs-12 col-md-10 col-md-offset-2");
